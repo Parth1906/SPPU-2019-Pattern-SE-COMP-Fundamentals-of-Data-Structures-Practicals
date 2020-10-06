de534f56615a6b263deb42507be275af2d6bc66f
Parth Rewoo <67362433+Parth1906@users.noreply.github.com>
2020-10-06 21:45:19 +0530

    Update GroupE_Practical29(C++).cpp

--- GroupE_Practical29(C++).cpp
+++ GroupE_Practical29(C++).cpp
@@ -1,3 +1,9 @@
+/*
+Queues are frequently used in computer programming, and a typical example is the creation of a job queue by an operating system. 
+If the operating system does not use priorities, then the jobs are processed in the order they enter the system.
+Write C++ program for simulating job queue. Write functions to add job and delete job from queue.
+*/
+
 #include <iostream>
 #define MAX 10
 using namespace std;
