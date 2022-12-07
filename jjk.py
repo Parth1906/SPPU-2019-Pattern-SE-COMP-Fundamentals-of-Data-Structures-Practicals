9ff16d6247731fe436ba3ca4c408b06c7e55cb76
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2022-12-07 23:35:51 +0530

    Changes

---
+++ jjk.py
@@ -0,0 +1,37 @@
+'''
+GroupA-5
+Write a python program to compute following operations on String:
+a) To display word with the longest length.
+b) To determine the frequency of occurance of particular character in the string
+c) To check whether given string is palindrome or not
+d) To display the index of first apperance of the substring
+e) To count the occurance of each string
+'''
+def length(sett, length):
+    longest=""
+    for i in range(0,length):
+        if len(longest) <= len(sett(i)):
+            longest = sett(i)
+        print(longest)
+        
+        
+def occurance(sett, length):
+    return 0
+def check(sett, length):
+    return 0
+def indexcheck(sett, length):
+    return 0
+def count(sett, length):
+    return 0
+
+
+if __name__ == "__main__":
+    string= input("Enter the string\n")
+    length= len(string)
+    sett= string.split()
+    length(sett, length)
+    occurance(sett, length)
+    check(sett, length)
+    indexcheck(sett, length)
+    count(sett, length)
+          
